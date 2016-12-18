b73a198eb75a70ed3e30f7dc7c1b9b8f73c752be
JohnRoz <yonimeiron99@gmail.com>
2016-12-18 21:26:13 +0200

    FINISHED ! :confetti_ball: :tada: :balloon:

--- ValidateDetails.js
+++ ValidateDetails.js
@@ -1,58 +1,181 @@
 function validate_details() {
+    var isValidUsername = is_valid_username();
+    var isValidEmail = is_valid_email();
+    var isValidPassword = is_valid_password();
+    var isPasswordConfirmed = is_password_confirmed();
+    var isFullNameValid = is_full_name_valid();
+    var isValidPhoneNum = is_valid_phone_num();
+    var isBirthDateReal = is_birth_date_real();
+    var hasAgreedToTerms = has_agreed_to_terms();
+
     if (
-        is_real_name_OK()
-        && has_agreed_to_terms()
-        && is_valid_password()
-        &&is_valid_username()
+        isValidUsername
+        && isValidEmail
+        && isValidPassword
+        && isPasswordConfirmed
+        && isFullNameValid
+        && isValidPhoneNum
+        && isBirthDateReal
+        && hasAgreedToTerms
     )
         alert("THANK'S FOR SIGNING UP !");
-
-    else
-        alert("CHECK THE DETAILS AGAIN...");
 }
 
-function is_real_name_OK() {
-    var realName = $("RealNameInput").val();
-    var check = /[a-zA-Z]/.test(realName);
-    return (check);
-}
+// *** REMEMBER THIS IF YOU ENTER THIS CODE IN THE FUTURE !!!! ***
+var currentYear = 2017;
 
-function has_agreed_to_terms() {
-    var isChecked = document.getElementById("termsAndConditionsCheckBox").checked;
-    return (isChecked);
-}
 
-function is_valid_password() {
-    var password = document.getElementById("passwordInput").value;
-
-    var containsSmallLetter = /[a-z]/.test(password);
-    var containsCapitalLetters = /[A-Z]/.test(password);
-    var containsDigit = /\d/.test(password);
-    var containsSpecial = /[^a-zA-Z\d]/.test(password);
-
-    if (!containsSmallLetter || !containsCapitalLetters || !containsDigit || !containsSpecial)
-        alert("Password needs to contain lowercase letters, uppercase letters, digits and special characters !");
-    else {
-        if (!(password.length >= 8))
-            alert("Password's length must be longer that 8 characters !");
-        else
-            return true;
-    }
-}
-
-function is_valid_username(){
+function is_valid_username() {
     var username = document.getElementById("usernameInput").value;
 
-    if(password.length>20){
-        alert("username's length mustn't be longer than 20 characters !")
+    // Check is the length of the username is between 2 and 20 characters.
+    if (username.length > 20 || username.length < 2) {
+        alert("username's length must be between 2 and 20 characters !")
         return false;
     }
     return true;
 }
 
-/*
-//LAMDA FUNCTION:
-$(document).ready(function(){
-    $("#signUpButton").onClick = validate_details;
-});
-*/
+function is_valid_email() {
+    var email = document.getElementById("emailAddressInput").value;
+
+    // Check if email address was entered.
+    if (email.length == 0) {
+        alert("You must enter your Email address.");
+        return false;
+    }
+
+    // Check if the email input string contains the '@' char.
+    if (!email.includes("@")) {
+        alert("Not valid email address ! Please re-enter your email address.");
+        return false;
+    }
+    return true;
+}
+
+function is_valid_password() {
+    var password = document.getElementById("passwordInput").value;
+    // Check if the password input is empty.
+    if (password.length == 0) {
+        alert("You must have a password.");
+        return false;
+    }
+
+    var containsSmallLetter = /[a-z]/.test(password);
+    var containsCapitalLetters = /[A-Z]/.test(password);
+    var containsDigit = /\d/.test(password);
+    var containsSpecial = /[^a-zA-Z\d\s]/.test(password);
+
+    // Check the password contains at least one lowercase letter, one uppercase letter, one digit and one special character.
+    if (!containsSmallLetter || !containsCapitalLetters || !containsDigit || !containsSpecial) {
+        alert("Password needs to contain lowercase letters, uppercase letters, digits and special characters !");
+        return false;
+    }
+
+
+    // Check the password's length is longer than 8 characters.
+    if (password.length < 8) {
+        alert("Password's length must be longer that 8 characters !");
+        return false;
+    }
+
+
+    return true;
+
+}
+
+function is_password_confirmed() {
+    var confirmedPassword = document.getElementById("confirmPasswordInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    // Check if password was confirmed.
+    if (confirmedPassword.length == 0) {
+        alert("You must confirm your password")
+        return false;
+    }
+
+    // Check if the value of the confirmPassword input is equal to the password input.
+    if (confirmedPassword.localeCompare(password) == 0)
+        return true;
+
+    alert("The 'confirm password' input must be identical to the 'password' input.");
+    return false;
+}
+
+function is_full_name_valid() {
+    var fullName = document.getElementById("FullNameInput").value;
+
+    // Check if the FullName input is empty.
+    if (fullName.length == 0) {
+        alert("You must enter your full name.");
+        return false;
+    }
+
+    var containsDigit = /\d/.test(fullName);
+    var containsSpecial = /[^a-zA-Z\d\s]/.test(fullName);
+
+    // If the user's name contains digits or special characters return false.
+    if (containsDigit || containsSpecial) {
+        alert("Your full name must contain only letters.");
+        return false;
+    }
+
+    return true;
+}
+
+function is_valid_phone_num() {
+
+    var phoneInput = document.getElementById("phoneNumInput").value
+
+    // Check if phone number was entered.
+    if (phoneInput.length == 0) {
+        alert("You must enter your phone number.")
+        return false;
+    }
+
+    var containsOnlyDigits = /^\d+$/.test(phoneInput);
+    // Check if the phone number doesn't contain onlt digits.
+    if (!containsOnlyDigits) {
+        alert("A phone number must contain only digits.")
+        return false;
+    }
+
+    return true;
+}
+
+function is_birth_date_real() {
+    var birthDate = document.getElementById("dateOfBirthInput").value;
+
+    // Check if the user entered a birth date.
+    if (birthDate.length == 0) {
+        alert("You must enter your birth date.");
+        return false;
+    }
+
+    var year = parseInt(birthDate.split("-")[0]);
+
+    // Check if the birth date is in the future
+    if (currentYear - year < 0) {
+        alert("Looks like we have a time traveller over here...");
+        return false;
+    }
+
+    // Check if the birth date is in the distant past.
+    if (currentYear - year >= 100) {
+        alert("come on...You're NOT more than 100 years old...");
+        return false;
+    }
+
+    return true;
+
+
+}
+
+function has_agreed_to_terms() {
+    var isChecked = document.getElementById("termsAndConditionsCheckBox").checked;
+    // Check if the user did not accept my ridiculous terms & conditions.
+    if (!isChecked)
+        alert("You must accept our Terms & Conditions.");
+
+    return (isChecked);
+}
